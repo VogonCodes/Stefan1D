54c40e2283e0691654b70a0382f35c1d1a4491ef
VogonCodes <bakocad@gmail.com>
2025-10-29 17:19:23 +0100

    redone all

--- Makefile
+++ Makefile
@@ -3,11 +3,11 @@ all: plot.png
 main: main.cpp
 	g++ -o main main.cpp
 
-data.dat anal.dat ym.dat &: main
+numSol.dat analSol.dat &: main
 	./main
 
-plot.png stefan.png ym.png: data.dat anal.dat ym.dat plotting.gpi
+plot.png: numSol.dat analSol.dat plotting.gpi
 	gnuplot plotting.gpi
 
 clean:
-	rm -f main data.dat anal.dat ym.dat plot.png stefan.png ym.png
+	main numSol.dat analSol.dat plot.png
--- main.cpp
+++ main.cpp
@@ -6,13 +6,22 @@
 #include <thread>
 #include <functional>
 #include <future>
-
-
+#include <cstdio>
 
 using namespace std;
 
 using Matrix = vector<vector<double>>;
 using Vector = vector<double>;
+
+// Define constants
+const double k = 2.0; // J/(m*s*K)
+const double c = 4.0e3; // J*(kg*K)
+const double rho = 1.0e3; // kg/m^3
+const double L = 320.0e3; // J/kg
+
+const double Ttop = 253.15; // K; = -20 C
+const double Tbot = 273.15; // K; =  0C
+const double Tmelt = 273.15; // K; =  0C
 
 void printVector(Vector vector, int size){
 	for(const auto& value : vector){
@@ -31,82 +40,27 @@ void printMatrix(Matrix matrix, int rows, int cols){
 	cout << endl;
 }
 
-void dumpMatrix(string filepath, Matrix& solution, int rows, int cols, double dt, double dx, bool bin=false){
-	// output binary file (sort of*) readable by gnuplot
-	// * skip first ?? bytes
-	ofstream file;
-	if (bin){
-		double t, x, T;
-		file.open(filepath, ios::binary);
+// Useful stuff
 
-		// no. of records
-		file.write(reinterpret_cast<const char*>(&rows), sizeof(int));
-		// no. of points per record
-		file.write(reinterpret_cast<const char*>(&cols), sizeof(int));
-
-		for (int j=0; j<rows; j++){
-			//file.write(reinterpret_cast<const char*>(&cols), sizeof(static_cast<int32_t>(cols)));
-			t = j*dt;
-			for (int i=0; i<cols; i++) {
-				x = i*dx;
-				T = solution[j][i];
-				file.write(reinterpret_cast<const char*>(&x), sizeof(double));
-				file.write(reinterpret_cast<const char*>(&T), sizeof(double));
-				file.write(reinterpret_cast<const char*>(&t), sizeof(double));
-			}
-		}
-	}
-	else{
-		file.open(filepath);
-		file << "# " << rows << " x " << cols << "; dt=" << dt << endl;
-		for (int j=0; j<rows; j++) {
-			for (int i=0; i<cols; i++) {
-				file << i*dx << " " << solution[j][i] << " " << j*dt << endl;
-			}
-			file << endl << endl;
-		}
-	}
-	file.close();
-}
-
-void dumpVector(string filepath, Vector& vector, int size, double dt, double dx) {
+void dumpTempVector(string filepath, Vector& vector, int size, double dx) {
 	ofstream file(filepath);
 	for (int i=0; i<size; i++) {
-		file << vector[i] << " " << 273.15 << " "  << i*dt << endl;
+		file << i*dx << " " << vector[i] << endl;
 	}
 }
 
-void Heat1D(Matrix& solution, double dx, int noCells, double noSteps, double timestep, double diffCoef, double tempTop, double tempBottom, Vector tempInit) {
-	// assumes solution and tempInit have the right dimensions (noCells x noSteps and noCells respectively)!
-	double c = diffCoef * timestep / (dx * dx);
-	solution[0][noCells-1] = tempBottom;
-	solution[0][0] = tempTop;
-	solution[0] = tempInit;
-
-	// Central differences
-	for (int j=1; j<noSteps; j++) {
-		solution[j][0] = tempTop;
-		solution[j][noCells-1] = tempBottom;
-		for (int i=1; i<noCells-1; i++) {
-			solution[j][i] = solution[j-1][i] + diffCoef*(solution[j-1][i+1] - 2*solution[j-1][i] + solution[j-1][i-1])*timestep/dx/dx;
-		}
+void forwardEuler2(Vector& y, Vector& y_old, size_t N, double a) {
+	// y(x_i, t_{n+1}) = y(x_{i+1}, t_n - delta t) + a * [ y(x_{i+1}, t_{n-1}) - 2*y(x_i, t_{n-1}) + y(x_{i-1}, t_{n-1}) ]
+	for (int i=1; i<N-1; i++) {
+		y[i] = y_old[i] + a*(y_old[i+1] - 2*y_old[i] + y_old[i-1]);
 	}
 }
 
-double findLambda(double dT, double latentHeat, double heatCap, double initGuess) {
-	const double TOL = 1.0e-15;
+double findLambda(double dT, double latentHeat, double heatCap, double initGuess, double TOL) {
 	double RHS = latentHeat * sqrt(M_PI) / (heatCap * dT);
 	int i = 0;
 
 	double lambda = initGuess, lambda_old = lambda + TOL;
-	/*// use fixed-point iteration
-	while (abs(lambda-lambda_old)>TOL) {
-		lambda_old = lambda;
-		lambda = exp(-lambda_old*lambda_old) / erf(lambda_old) / RHS;
-		if (i % 10000000 ==0) cout << i << " " << lambda << " " << lambda_old << endl;
-		i++;
-	}*/
-
 	// Newton
 	double f,fprime;
 	double ee, erflm;
@@ -131,76 +85,66 @@ double findLambda(double dT, double latentHeat, double heatCap, double initGuess
 	return lambda;
 }
 
-void StefanAnal(Matrix& solution, Vector& ym, int N, int T, double dx, double dt, double Ttop, double deltaT, double thermCond, double heatCap, double density, double latentHeat) {
-	double lambda = findLambda(deltaT, latentHeat, heatCap, 0.3);
-	double erflm = erf(lambda);
-	double thermDiff = thermCond/density/heatCap;
-	double eta;
-	ym[0] = 0.0;
+void StefanProblem1D(Vector& numSol, Vector& analSol, Vector Tinit, size_t& numSolSize, size_t& analSolSize, double maxTime, double& dt, double& dx, double ymInit) {
+	double time = dt;
+	double ym = ymInit, ymOld = ymInit;
+	double kappa = k/rho/c;
+	double lambda = findLambda(20, L, c, 0.3, 1e-15);
+	double maxdx = 0.005;
+	double dxOld = dx;
+	dx = 0.001;
 
-	for (int i=0; i<N; i++) solution[0][i] = Ttop + deltaT;
-	for (int j=1; j<T; j++){
-		ym[j] = ym[j-1] + lambda * sqrt(thermDiff/j*dt)*dt;
-		for (int i=0; i<N; i++) {
-			if (i*dx <= ym[j]){
-				eta = i*dx/2/sqrt(thermDiff * j*dt);
-				solution[j][i] = Ttop + deltaT*erf(eta)/erflm;
-				//if (solution[j][i] > (Ttop + deltaT)) solution[j][i] = Ttop + deltaT;
-			}
-			else solution[j][i] = Ttop + deltaT;
-		}
+	Vector oldSol(numSolSize);
+	Vector tmp;
+	int numSolSizeOld = numSolSize;
+	oldSol = Tinit;
+	numSol = Tinit;
+
+	// Numerical solution + solidification interface depth
+	while (time < maxTime) {
+		ymOld = ym;
+		oldSol = numSol;
+		dxOld = dx;
+		numSolSizeOld = numSolSize;
+		// update solidification interface depth
+		ym = ymOld + lambda * sqrt(kappa/time)*dt;
+
+
+		// solve HEq for current time
+		forwardEuler2(numSol, oldSol, numSolSize, kappa*dt/dx/dx);
+		
+		time += dt;
+	}
+
+	// Analytical solution
+	double dT = Tmelt-Ttop;
+	double erfLambda = erf(lambda);
+	double sqr = sqrt(kappa*time);
+	analSol.resize(numSolSize);
+	analSolSize = numSolSize;
+	for (int i=0; i<numSolSize; i++) {
+		// for y<ym: theta = erf(eta)/erf(lambda) -> T = T0 + (Tm-T0)*erf(y/(2*sqrt(kappa*t)))/erf(lambda)
+		if (ym > i*dx) analSol[i] = Ttop + dT*erf(i*dx/2/sqr)/erfLambda;
+		else analSol[i] = Tmelt;
 	}
 }
 
-void solidifBound(Vector& ym, int T, double dt, double deltaT, double thermCond, double heatCap, double density, double latentHeat){
-	double lambda = findLambda(deltaT, latentHeat, heatCap, 0.3);
-	double thermDiff = thermCond/density/heatCap;
+// Useful stuff
+int main() {
+	size_t N = 3;
+	size_t M = 0;
+	double dx = 0.001;
+	double dt = 0.1;
+	double ymInit = N*dx;
+	Vector solution(N,0.0), solAn;
+	// Linear initial condition
+	Vector Tinit(N, 273.15);
+	for (int i=0; i<N; i++) Tinit[i] = Ttop + i*(Tmelt-Ttop)/M;
 
-	for (int j=1; j<T; j++) {
-		ym[j] = ym[j-1] + lambda*sqrt(thermDiff/j*dt)*dt;
-	}
-}
-
-int main(){
-	/* Define stuff */
-	const double k = 2.0; // J/(m*s*K)
-	const double c = 4.0e3; // J*(kg*K)
-	const double rho = 1.0e3; // kg/m^3
-	const double L = 320.0e3; // J/kg
-
-	const double Ttop = 253.15; // K; = -20 C
-	const double Tbot = 273.15; // K; =  0C
-
-	const double dt = 0.1; // s; temporal discretisation
-	const double height = 0.05; // m
-	const int T = 100;//8640000 s = 10 days; max time, non-dimensional
-	const int N = 100; // spatial discretisation degree
-
-	double dx = height / (N-1);
-
-	Matrix solution(T,Vector(N,0.0));
-	Matrix solutionAnal(T,Vector(N,0.0));
-	Vector Tini(N,Tbot);
-	Vector ym(T, 0.0);
-
-	/* Create linear initial temperature profile */
-	//double dT = 20.0/(N-1);
-	//for (int i=0;i<N;i++) Tini[i] = 253.15 + i*dT;
-
-	Tini[0] = Ttop;
-	//printVector(Tini,N);
-
-	// create threads to solve stuff
-	auto thrHEq1D = async(launch::async, Heat1D, ref(solution),dx,N,T,dt,k/(rho*c), Ttop, Tbot, Tini);
-	auto thrStefAn = async(launch::async, StefanAnal,ref(solutionAnal), ref(ym), N, T, dx, dt, Ttop, Tbot-Ttop, k, c, rho, L);
-	// once computed, start saving to file
-	auto thrHEq1DWrite = async(launch::async, [&]() {
-			thrHEq1D.get();
-			dumpMatrix("data.dat", ref(solution), T, N, dt, dx, true);
-			});
-	auto thrStefAnWrite = async(launch::async, [&]() {
-			thrStefAn.get();
-			dumpMatrix("anal.dat", ref(solutionAnal), T, N, dt, dx, true);
-			dumpVector("ym.dat", ym, T, dt, dx);
-			});
+	StefanProblem1D(solution, solAn, Tinit, N, M, 5.0, dt, dx, ymInit);
+	printVector(solAn,M);
+	printVector(solution, N);
+	dumpTempVector("initialCond.dat", Tinit, N, 0.001);
+	dumpTempVector("numSol.dat", solution, N, 0.001);
+	dumpTempVector("analSol.dat", solAn, N, 0.001);
 }
